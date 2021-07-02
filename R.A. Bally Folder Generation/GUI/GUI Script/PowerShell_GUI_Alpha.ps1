6f56cd7f8f54ded227b6d0b2fb90cde523ecb6c6
blurmcclure18 <blurmcclure16@gmail.com>
2021-07-02 13:05:43 -0500

    Made GUI Changes for better Spacing

--- R.A. Bally Folder Generation/GUI/GUI Script/PowerShell_GUI_Alpha.ps1
+++ R.A. Bally Folder Generation/GUI/GUI Script/PowerShell_GUI_Alpha.ps1
@@ -7,48 +7,48 @@ $inputXML = @'
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:PowerShell_GUI"
         mc:Ignorable="d"
-        Title="Window1" Height="450" Width="800" Background="#FF252526">
+        Title="Window1" Height="450" Width="656" Background="#FF252526">
     <Grid x:Name="Grid_Grid" HorizontalAlignment="Center" VerticalAlignment="Stretch">
         <Grid.RowDefinitions>
             <RowDefinition Height="Auto"/>
-            <RowDefinition Height="Auto" MinHeight="80"/>
-            <RowDefinition Height="Auto" MinHeight="80"/>
+            <RowDefinition Height="Auto" MinHeight="83"/>
+            <RowDefinition Height="Auto" MinHeight="77"/>
             <RowDefinition Height="Auto" MinHeight="60"/>
-            <RowDefinition Height="Auto" MinHeight="50"/>
-            <RowDefinition Height="Auto" MinHeight="20"/>
+            <RowDefinition Height="Auto" MinHeight="65"/>
+            <RowDefinition Height="Auto" MinHeight="5"/>
         </Grid.RowDefinitions>
         <Grid.ColumnDefinitions>
             <ColumnDefinition Width="Auto"/>
         </Grid.ColumnDefinitions>
-        <TextBox x:Name="FirstName_Text" HorizontalAlignment="Left" Margin="10,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="110" Height="18"/>
+        <TextBox x:Name="FirstName_Text" HorizontalAlignment="Left" Margin="10,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="109" Height="18"/>
         <Label x:Name="FirstName_Label" Content="First Name" HorizontalAlignment="Left" Margin="31,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="67"/>
-        <Label x:Name="Middle_Label" Content="Middle" HorizontalAlignment="Left" Margin="140,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="47"/>
-        <Label x:Name="LastName_Label" Content="Last Name" HorizontalAlignment="Left" Margin="227,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="66"/>
-        <Label x:Name="DateOfBirth_Label" Content="Date Of Birth" HorizontalAlignment="Left" Margin="525,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="79"/>
-        <Label x:Name="Age_Label" Content="Age" HorizontalAlignment="Left" Margin="624,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="31"/>
-        <Label x:Name="Spouse_Label" Content="Spouse" HorizontalAlignment="Left" Margin="401,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="49"/>
-        <Label x:Name="Card_Label" Content="Card?" HorizontalAlignment="Left" Margin="677,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="40"/>
-        <TextBox x:Name="Middle_Text" HorizontalAlignment="Left" Margin="155,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="16" Height="18"/>
-        <TextBox x:Name="LastName_Text" HorizontalAlignment="Left" Margin="197,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="125" Height="18"/>
-        <TextBox x:Name="Spouse_Text" HorizontalAlignment="Left" Margin="380,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="90" Height="18"/>
-        <TextBox x:Name="DateOfBirth_Text" HorizontalAlignment="Left" Margin="525,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="80" Height="18"/>
-        <TextBox x:Name="Age_Text" HorizontalAlignment="Left" Margin="630,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="20" Height="18"/>
-        <TextBox x:Name="Card_Text" HorizontalAlignment="Left" Margin="685,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="25" Height="18"/>
-        <TextBox x:Name="StreetAddress_Text" HorizontalAlignment="Left" Margin="52,0,0,0" Grid.Row="1" TextWrapping="Wrap" Width="270" VerticalAlignment="Center" Height="18"/>
-        <TextBox x:Name="City_Text" HorizontalAlignment="Left" Margin="358,0,0,0" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="120" Height="18"/>
-        <TextBox x:Name="State_Text" HorizontalAlignment="Left" Margin="556,0,0,0" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="18" Height="18"/>
-        <TextBox x:Name="ZipCode_Text" HorizontalAlignment="Left" Margin="656,0,0,0" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="44" Height="18"/>
-        <Label x:Name="StreetAddress_Label" Content="Street Address" HorizontalAlignment="Left" Margin="144,4,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="86"/>
-        <Label x:Name="City_Label" Content="City" HorizontalAlignment="Left" Margin="403,4,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="30"/>
-        <Label x:Name="State_Label" Content="State" HorizontalAlignment="Left" Margin="546,4,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="37"/>
-        <Label x:Name="ZipCode_Label" Content="Zip Code" HorizontalAlignment="Left" Margin="649,4,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="58"/>
-        <TextBox x:Name="Companies_Text" HorizontalAlignment="Center" Grid.Row="2" TextWrapping="Wrap" VerticalAlignment="Center" Width="512" Height="18"/>
-        <Label x:Name="Companies_Label" Content="Companies On File" HorizontalAlignment="Center" Grid.Row="2" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="110"/>
-        <TextBox x:Name="ClientFolderPath_Text" HorizontalAlignment="Center" Grid.Row="3" Text="&#xD;&#xA;" TextWrapping="Wrap" VerticalAlignment="Top" Width="512" Height="18" Margin="0,30,0,0"/>
-        <Label x:Name="ClientFolderPath_Label" Content="Client File Path" HorizontalAlignment="Center" Grid.Row="3" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="89" Margin="0,4,0,0"/>
-        <Button x:Name="OpenClientFolder_Button" Content="Open File" HorizontalAlignment="Left" Margin="640,28,0,0" Grid.Row="3" VerticalAlignment="Top" Height="20" Width="57"/>
-        <Button x:Name="SearchClient_Button" Content="Search Client" HorizontalAlignment="Left" Margin="227,0,0,0" Grid.Row="4" VerticalAlignment="Center" Height="20" Width="73"/>
-        <Button x:Name="NewClient_Button" Content="New Client" HorizontalAlignment="Left" Margin="418,0,0,0" Grid.Row="4" VerticalAlignment="Center" Height="20" Width="73"/>
+        <Label x:Name="Middle_Label" Content="Middle" HorizontalAlignment="Left" Margin="112,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="48"/>
+        <Label x:Name="LastName_Label" Content="Last Name" HorizontalAlignment="Left" Margin="183,41,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="66"/>
+        <Label x:Name="DateOfBirth_Label" Content="Date Of Birth" HorizontalAlignment="Left" Margin="394,44,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="79"/>
+        <Label x:Name="Age_Label" Content="Age" HorizontalAlignment="Left" Margin="478,44,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="31"/>
+        <Label x:Name="Spouse_Label" Content="Spouse" HorizontalAlignment="Left" Margin="312,44,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="50"/>
+        <Label x:Name="Card_Label" Content="Card?" HorizontalAlignment="Left" Margin="506,44,0,0" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="40"/>
+        <TextBox x:Name="Middle_Text" HorizontalAlignment="Left" Margin="128,71,0,0" TextWrapping="Wrap" Width="16"/>
+        <TextBox x:Name="LastName_Text" HorizontalAlignment="Left" Margin="153,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="126" Height="18"/>
+        <TextBox x:Name="Spouse_Text" HorizontalAlignment="Left" Margin="291,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="91" Height="18"/>
+        <TextBox x:Name="DateOfBirth_Text" HorizontalAlignment="Left" Margin="393,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="81" Height="18"/>
+        <TextBox x:Name="Age_Text" HorizontalAlignment="Left" Margin="483,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="21" Height="18"/>
+        <TextBox x:Name="Card_Text" HorizontalAlignment="Left" Margin="513,71,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="26" Height="18"/>
+        <TextBox x:Name="StreetAddress_Text" HorizontalAlignment="Left" Margin="10,0,0,0" Grid.Row="1" TextWrapping="Wrap" Width="270" VerticalAlignment="Center" Height="18"/>
+        <TextBox x:Name="City_Text" HorizontalAlignment="Left" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="120" Height="18" Margin="304,0,0,0"/>
+        <TextBox x:Name="State_Text" HorizontalAlignment="Left" Margin="443,0,0,0" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="29" Height="18"/>
+        <TextBox x:Name="ZipCode_Text" HorizontalAlignment="Left" Margin="495,0,0,0" Grid.Row="1" TextWrapping="Wrap" VerticalAlignment="Center" Width="44" Height="18"/>
+        <Label x:Name="StreetAddress_Label" Content="Street Address" HorizontalAlignment="Left" Margin="102,5,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="86"/>
+        <Label x:Name="City_Label" Content="City" HorizontalAlignment="Left" Margin="349,5,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="30"/>
+        <Label x:Name="State_Label" Content="State" HorizontalAlignment="Left" Margin="440,5,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="36"/>
+        <Label x:Name="ZipCode_Label" Content="Zip Code" HorizontalAlignment="Left" Margin="488,5,0,0" Grid.Row="1" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="58"/>
+        <TextBox x:Name="Companies_Text" HorizontalAlignment="Center" Grid.Row="2" TextWrapping="Wrap" VerticalAlignment="Center" Width="562" Height="18"/>
+        <Label x:Name="Companies_Label" Content="Companies On File" HorizontalAlignment="Center" Grid.Row="2" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="110" Margin="0,5,0,0"/>
+        <TextBox x:Name="ClientFolderPath_Text" HorizontalAlignment="Left" Grid.Row="3" Text="&#xD;&#xA;" TextWrapping="Wrap" VerticalAlignment="Top" Width="512" Height="18" Margin="11,30,0,0"/>
+        <Label x:Name="ClientFolderPath_Label" Content="Client File Path" HorizontalAlignment="Center" Grid.Row="3" VerticalAlignment="Top" Foreground="#FF007ACC" Height="26" Width="90" Margin="0,4,0,0"/>
+        <Button x:Name="OpenClientFolder_Button" Content="Open File" Margin="527,30,0,0" Grid.Row="3" VerticalAlignment="Top" Height="18"/>
+        <Button x:Name="SearchClient_Button" Content="Search Client" HorizontalAlignment="Left" Margin="169,0,0,0" Grid.Row="4" VerticalAlignment="Center" Height="20" Width="73"/>
+        <Button x:Name="NewClient_Button" Content="New Client" HorizontalAlignment="Left" Margin="339,0,0,0" Grid.Row="4" VerticalAlignment="Center" Height="20" Width="73"/>
     </Grid>
 </Window>
 '@
